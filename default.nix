0d2b2bc1e356c23a1b78405a8139f6c249523c97
Vladimir Kirillov <proger@wilab.org.ua>
2014-03-11 22:07:55 +0200

    update cabal & nix

--- default.nix
+++ default.nix
@@ -1,14 +1,15 @@
-{ pkgs ? (import <nixpkgs> {}), ... }:
+{ pkgs ? (import <nixpkgs> {}), hs ? pkgs.haskellPackages, ... }:
 let
-  haskellPackages = pkgs.haskellPackages;
-  inherit (haskellPackages) cabal;
-  cabalInstall = haskellPackages.cabalInstall_1_18_0_3;
+  inherit (hs) cabal hdevtools time text;
+  cabalInstall = hs.cabalInstall_1_18_0_3;
 in cabal.mkDerivation (self: {
   pname = "hiak";
   version = "0.1.0.0";
   src = ./.;
-  buildDepends = [
-  ];
+
+  buildDepends = [ time text ];
+  extraLibraries = [ hdevtools ];
   buildTools = [ cabalInstall ];
+
   enableSplitObjs = false;
 })
