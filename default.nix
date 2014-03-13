94345527497e75487f632b2a2300579c06432938
Vladimir Kirillov <proger@wilab.org.ua>
2014-03-13 13:07:47 +0200

    update deps

--- default.nix
+++ default.nix
@@ -1,13 +1,13 @@
 { pkgs ? (import <nixpkgs> {}), hs ? pkgs.haskellPackages, ... }:
 let
-  inherit (hs) cabal hdevtools time text;
+  inherit (hs) cabal hdevtools time text hashable SHA bytestring;
   cabalInstall = hs.cabalInstall_1_18_0_3;
 in cabal.mkDerivation (self: {
   pname = "hiak";
   version = "0.1.0.0";
   src = ./.;
 
-  buildDepends = [ time text ];
+  buildDepends = [ time text hashable bytestring SHA ];
   extraLibraries = [ hdevtools ];
   buildTools = [ cabalInstall ];
 
